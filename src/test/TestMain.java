f25c0213ed6002ec4fcea4ed46a48bc050e5107f
Lukas <lukasduechting@mailbox.org>
2022-05-17 16:02:30 +0200

    .

---
+++ src/test/TestMain.java
@@ -0,0 +1,9 @@
+package test;
+
+public class TestMain {
+    public static void main(String[] args){
+        AppMain.execute_computation_exercises(3,5);
+        AppMain.execute_computation_exercises(5,5);
+        AppMain.execute_computation_exercises(7,5);
+    }
+}
