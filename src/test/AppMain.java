0d36394428b96b489e2eaa0cbb040e9cafc82955
Lukas <lukasduechting@mailbox.org>
2022-05-17 16:01:32 +0200

    logical operators

--- src/test/AppMain.java
+++ src/test/AppMain.java
@@ -8,6 +8,13 @@ public class AppMain{
 		System.out.println("hallo welt");
 		System.out.println("print args array: " + Arrays.toString(args));
 
+		var nums = scan_numbers();
+
+		execute_computation_exercises(nums[0], nums[1]);
+	}
+
+	static int[] scan_numbers(){
+
 		Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
 
 		System.out.println("Enter two numbers to add: ");
@@ -16,10 +23,23 @@ public class AppMain{
 		System.out.println("second number: ");
 		int second_number = Integer.parseInt(myScanner.nextLine());  // Read user input
 
+		return new int[]{first_number, second_number};
+	}
+	public static void execute_computation_exercises(int first_number, int second_number){
 
-		int result = ComputeExercise.add(first_number, second_number);
+		int sum = ComputeExercise.add(first_number, second_number);
+		System.out.println(first_number + " + " + second_number + " is " + sum);
 
-		System.out.println(first_number + " + " + second_number + " is " + result);
+
+		if (ComputeExercise.equal(first_number, second_number)){
+			System.out.println(first_number + " ist gleich " + second_number);
+		} else{
+			if(ComputeExercise.smaller(first_number, second_number)){
+				System.out.println(first_number + " ist kleiner als " + second_number);
+			} else {
+				System.out.println(first_number + " ist größer als " + second_number);
+			}
+		}
 	}
 }
 
--- src/test/ComputeExercise.java
+++ src/test/ComputeExercise.java
@@ -4,4 +4,11 @@ public class ComputeExercise {
     static int add(int a, int b){
         return a+b;
     }
+
+    static boolean smaller(int a, int b){
+        return a<b;
+    }
+    static boolean equal(int a, int b){
+        return a==b;
+    }
 }
