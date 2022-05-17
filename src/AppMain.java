0a3f5436a8a8387d1e57e2d35e5b8ded6a7893e3
Lukas <lukasduechting@mailbox.org>
2022-05-17 13:06:45 +0200

    add dummy program

---
+++ src/AppMain.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class AppMain{
+	public static void main(String[] args){
+		System.out.println("hallo welt");
+		System.out.println("print args array: " + Arrays.toString(args));
+	}
+}
